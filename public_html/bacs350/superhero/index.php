ad7a21fd95a42eaffe8d5aa6aa9ad8bafdebdcbb
Mitch Haley <30455646+Pandabnana21@users.noreply.github.com>
2019-09-13 15:10:40 -0600

    456

--- public_html/bacs350/superhero/index.php
+++ public_html/bacs350/superhero/index.php
@@ -1,74 +1,32 @@
+<?php
 
-<!doctype html>
-<html lang="en">
+    /*
+        Superhero Project Workshop
+    */
 
-<head>
+    // Get the render_page and render_card functions
+    include 'views.php';
 
-    <meta charset="utf-8">
-    <title>Porject 3 and Demo 7</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="http://shrinking-world.com/static/images/unc/favicon.ico">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
 
-</head>
+    // Set custom settings
+    $site_title = 'UNC BACS 350';
+    $page_title = 'Superhero Gallery';
 
-<body>
+    $card1 = render_card("Captain America", "Classic America hero archtype");
+    $card2 = render_card("Iron Man", "Billionaire Entrpreneur");
+    $card3 = render_card("Batman", " Entrpreneur Bat");
 
-<header>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-8">
-                <h1>Superhero Gallery</h1>
-                <h2>Card Lesson</h2>
-            </div>
-            <div class="logo col-sm-4">
-                <div class="pull-right">
-                    <img class="img-rounded img-responsive"
-                         src="https://shrinking-world.com/static/images/unc/Bear.200.png" alt="UNC Bear" width="150px">
-                </div>
+
+    $content =  '
+        <div class="container-fluid">
+            <div class="row">
+                ' . $card1 . $card2 . $card3 .'
             </div>
         </div>
-    </div>
-</header>
-
-<main>
-    <div class="container-fluid">
-        <div class="row">
-
-           
-
-            <div class="card" style="width: 18rem;">
-                <img src="image1.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-                </div>
-
-            <div class="card" style="width: 18rem;">
-                <img src="image2.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-                </div>
-        
-                <div class="card" style="width: 18rem;, height: 18rem">
-                <img src="image3.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-                </div>
-    </div>
-    </div>
-</main>
+    ';
 
 
+    // Create HTML and output the page
+    echo render_page($site_title, $page_title, $content);
 
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
+?>
