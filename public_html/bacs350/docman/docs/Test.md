d3c2edc26ffc77253687fc4f65009f7d601f8b15
Mitch Haley <30455646+Pandabnana21@users.noreply.github.com>
2019-09-27 14:54:00 -0600

    j

---
+++ public_html/bacs350/docman/docs/Test.md
@@ -0,0 +1,14 @@
+# DocMan Test
+
+This page is for testing the DocMan project during development.
+
+
+## Links
+* [Test locally](http://localhost/bacs350/docman)
+* [Test remotely](bears98.com/bacs350/docman)
+* [Sensei](https://shrinking-world.com/unc/bacs350)
+* [Missing Page](xxx.md)
+* [Missing Document](bears98.com/bacs350/docman/doc.php?xxx.md)
+
+* []()
+
---
+++ public_html/bacs350/docman/docs/index.php
@@ -0,0 +1,60 @@
+      <!DOCTYPE html>
+            <html lang="en">
+                <head>
+
+                    <meta charset="UTF-8">
+                    <title>Project #4 - Project Planner</title>
+
+                    <link rel="icon" type="image/x-icon" href="/bacs350/favicon.ico">
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+                    <link rel="stylesheet" href="style.css">
+
+                </head>
+              <body>
+<h1>Mitch Markdown Cheatsheet</h1>
+<h2>Mitch Project Planner</h2>
+                
+
+<h3><em>Table of Projects</em></h3>
+<table style="width: 20%;" border="5" cellpadding="5">
+<tbody>
+<tr>
+<td>Project 1</td>
+<td>Setup Web Hosting</td>
+</tr>
+<tr>
+<td>Project 2</td>
+<td><a href="https://bears98.com/bacs350/project/02/">Here</a></td>
+</tr>
+<tr>
+<td>Project 3</td>
+<td><a href="https://bears98.com/bacs350/project/03/">Here</a></td>
+</tr>
+<tr>
+<td>Project 4</td>
+<td><a href="http://bears98.com/bacs350/planner/">Here</a></td>
+</tr>
+</tbody>
+</table>
+                
+
+<h3>Skill List</h3>
+<ul>
+<li>Html, CSS</li>
+<li>PHP</li>
+</ul>
+<h3>To do List</h3>
+<ol>
+<li>Marketing Homework</li>
+<li>Study for finance exam</li>
+</ol>
+<h3>GitHub Repos</h3>
+<a href="https://github.com/Mark-Seaman/UNC-BACS350-2019-Fall/tree/master/public_html/bacs350"><p>Seaman Repo</p></a>
+<a href="https://github.com/Pandabnana21/UNC-BACS350-2019-Fall"><p>My Repo</p></a>
+
+<h2>Preformatted</h2>
+<pre><code>def function():
+    try_this()</code></pre>
+                </body>
+            </html>
+        
