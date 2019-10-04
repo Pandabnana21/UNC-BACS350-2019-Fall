876fe067ac383915367974bbbc9d642fc0589e82
Mitch Haley <30455646+Pandabnana21@users.noreply.github.com>
2019-10-04 15:04:04 -0600

    css try php

--- public_html/bacs350/subscriber/step1.php
+++ public_html/bacs350/subscriber/step1.php
@@ -1,11 +1,14 @@
+<html>
 <head><link rel="stylesheet" href="styles.css"></head>
+    <style>background: radial-gradient(#f7b5b5, #6498fc);
+</style>
 <h1>Subscriber Database</h1>
 
 <p>
     <a href="index.php">Subscriber List Code</a>
 </p>
 
-<h1>CONNECTED to Subscriber Database</h1>
+<h2>CONNECTED to Subscriber Database</h2>
 
 <?php
 
@@ -16,11 +19,11 @@
     $username = 'bearsnin';
     $password = '$Tanley2121';
     
-    echo "<h1>$db_connect</h1>";
+    echo "<h3>$db_connect</h3>";
     
     $db = new PDO($db_connect, $username, $password);
 
-    echo "<h1>CONNECTED</h1>";
+    echo "<h2>CONNECTED</h2>";
 
 
     // Get a list of records into an array
@@ -29,7 +32,7 @@
     $statement->execute();
     $subscribers = $statement->fetchAll();
 
-    echo "<h1>QUERY DONE</h1>";
+    echo "<h2>QUERY DONE</h2>";
 
     // Create an HTML list on the output
     echo '<ul>';
@@ -38,6 +41,7 @@
     }
     echo '</ul>';
 
-    echo "<h1>LIST CREATED</h1>";
+  
 
 ?>
+</html>
