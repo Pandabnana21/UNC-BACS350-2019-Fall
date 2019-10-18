0040f9bff7655452ec22126cba561e8f4275383b
Mitch Haley <30455646+Pandabnana21@users.noreply.github.com>
2019-10-18 15:22:46 -0600

    notes done

---
+++ public_html/bacs350/notes/index.php
@@ -0,0 +1,26 @@
+<?php
+
+    // Code to define functions
+    require_once 'views.php';
+    require_once 'notes_views.php';
+    require_once 'notes_db.php';
+
+
+    // List subscriber records
+    $list = render_notes(list_notes ($db));
+
+    
+    // Button to go to other views
+    $add_button = '<p><a class="button" href="insert.php">Add Note</a></p>';
+
+    
+    // Display the HTML in the page
+    $intro = '
+      
+         
+    ';
+    $content = "$intro $add_button $list";
+
+    // Show the page
+    echo render_page( $content);
+?>
