3849beb75aa652799c8f20f94faacafbbddb09f7
Mitch Haley <30455646+Pandabnana21@users.noreply.github.com>
2019-10-25 15:30:41 -0600

    broken code

--- public_html/bacs350/review/update.php
+++ public_html/bacs350/review/update.php
@@ -1,36 +1,39 @@
 <?php
 
     // Code to define functions
-    require_once 'views.php';
+   // require_once 'views.php';
     require_once 'review_views.php';
     require_once 'review_db.php';
 
 
     // Pick out the inputs
     $id    = filter_input(INPUT_POST, 'id');
-    $title = filter_input(INPUT_POST, 'design');
-    $body  = filter_input(INPUT_POST, 'body');
-    $date  = filter_input(INPUT_POST, 'date');
+    $designer = filter_input(INPUT_POST, 'designer');
+    $url  = filter_input(INPUT_POST, 'url');
+    $report  = filter_input(INPUT_POST, 'report');
+$score  = filter_input(INPUT_POST, 'score');
+$date = filter_input(INPUT_POST, 'date');
+
 
       date_default_timezone_set("America/Denver");
 $date  = date('Y-m-d g:i:s a');
         
 
     // Gather user input with a form
-    if ($title == '' || $body == '' || $date == '') {
+    if ($id == '' || $designer == '' || $date == '' || $url == '' || $report == '' || $score == '' ) {
         
         // Form view to add notes
         $id = filter_input(INPUT_GET, 'id');
-        $note = get_note($db, $id);
-        $edit_form = edit_note_form($note);
+        $review = get_review($db, $id);
+        $edit_review= edit_review_form($review);
 
         // Display the HTML in the page
-        echo render_page('UNC BACS 350', "Edit Note", $edit_form);
+        echo render_page('UNC BACS 350', "Edit Note", $edit_review);
     }
     else {
         
         // Add record and return to list
-        if (update_note ($db, $id, $title, $body, $date))
+        if (update_review($db, $id, $designer, $url, $date, $report, $score))
         {
             header("Location: index.php");
         }
